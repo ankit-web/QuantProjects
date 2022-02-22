59d19d3cb8ccc51ac16f04e2c4f6b7ec40603441
Ankit <56376967+ankit-web@users.noreply.github.com>
2022-02-22 19:02:01 +0800

    Update yeidCurve.py

--- Nielson Seigal Spot Curve generation/code/yeidCurve.py
+++ Nielson Seigal Spot Curve generation/code/yeidCurve.py
@@ -372,11 +372,11 @@ def main():
     ## CSV file generation
     logging.info(msg="Calibration completed")
     output_parameters.to_csv(params.FileOutpath.value+"/NS_paramters.csv")
-    logging.info("investcloud_NS_paramters.csv file generated sucessfully")
-    output_NS.to_csv(params.FileOutpath.value+"investcloud_NS_predicted_spot.csv")
-    logging.info("investcloud_NS_predicted_spot.csvfile generated sucessfully")
+    logging.info("NS_paramters.csv file generated sucessfully")
+    output_NS.to_csv(params.FileOutpath.value+"NS_predicted_spot.csv")
+    logging.info("NS_predicted_spot.csvfile generated sucessfully")
     statsOutput.to_csv(params.FileOutpath.value + "/NSActualPreredictedStatsComp.csv")
-    logging.info("investcloud_NSActualPreredictedStatsComp.csv generated sucessfully")
+    logging.info("NSActualPreredictedStatsComp.csv generated sucessfully")
     logging.info('Completed')
 
     ### 4 - Simulate prices of artificial bonds
